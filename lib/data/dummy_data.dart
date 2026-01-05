65289ff645f8176a11c43ff0d008031fe86009f5
saleem Akhtar <saleemm1313@gmail.com>
2026-01-05 13:26:07 +0500

    project setups models,data,widgets

---
+++ lib/data/dummy_data.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:meals/models/category.dart';
+
+const availableCategories = [
+  Category(id: 'c1', title: 'German', color: Colors.teal),
+  Category(id: 'c2', title: 'Italian', color: Colors.brown),
+  Category(id: 'c3', title: 'Quick & Easy', color: Colors.green),
+  Category(id: 'c4', title: 'Hamburger', color: Colors.amber),
+  Category(id: 'c5', title: 'French', color: Colors.deepPurple),
+  Category(id: 'c6', title: 'Asian', color: Colors.pinkAccent),
+  Category(id: 'c7', title: 'Summer', color: Colors.lightBlue),
+  Category(id: 'c8', title: 'Pakistani', color: Colors.blueGrey),
+  Category(id: 'c9', title: 'Dessi', color: Colors.tealAccent),
+  Category(id: 'c10', title: 'chinees', color: Colors.red),
+];
